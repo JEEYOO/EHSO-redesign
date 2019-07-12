fe36d985d2583a3ba755d968df1b2e130afb07b1
Jee Yoo <yoojw94@gmail.com>
2019-07-12 19:20:24 +1000

    javascript 1

---
+++ fbform.js
@@ -0,0 +1,7 @@
+var form = document.getElementById("fbsubmit");
+
+document.getElementById("fbreturn").addEventListener("click", function () {
+  form.submit();
+});
+
+// Literally , this js exists for submission of fb=feedback.html page. Click will activate this fucntion 
